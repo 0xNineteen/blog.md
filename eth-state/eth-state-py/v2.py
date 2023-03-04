ea99e31bfed8455e9cf880e200fd67c29759c5f0
0xNineteen <0x39015319@gmail.com>
2023-03-04 13:18:14 -0500

    clean up

--- eth-state/eth-state-py/v2.py
+++ eth-state/eth-state-py/v2.py
@@ -1,60 +1,6 @@
 #%%
 from utils import *
 
-# chain:
-# get parent state [x]
-# modify state [x]
-# compute new account_hash
-# hash(account, block_uniq) - whats block_uniq? [tx_root, parent_hash]
-# cant be state_root bc its derived from the account hashes
-# cant be block_root bc its derived from the state_root
-# can it be the tx_root? ... [todo: add tx_root to block]
-#   in POW you can have two blocks which have the same tx_root, parent_hash, but different nonce
-#   and then you have a fake fork (same chain differnt hash) -- thats probs pretty unlikley so i think its ok
-# store new serialized account
-# generate state_root [x]
-# get parent_root
-# remove the old account_hash
-# insert the new account_hash ()
-# compute the new root hash
-# store new_root_hash => [new-account-hashes]
-
-# eg,
-# first tx occurs
-# db is empty
-# create account
-# key = hash([address, tx_root, parent_root])
-#   # this ALONE wont work bc this requires to know the tx_root+parent_root of the most recently updated block
-#   # combine it with (HASH, ADDRESS) and we have lookup working
-# ...
-# address = hash(serialized(account)) <- can just do this if we have lookup handled by the list
-# account_keys = [(key, address)]
-# db.put(state_root, account_keys) # state_root => [(account_hash, address)]
-# db.put(key, account) # hash => account
-# second tx occurs (which updates address)
-# get the list of previous accounts
-# addresses = state_db.get(parent_block.state_root)
-# key, _ = find(address, addresses)
-# account = account_db.get(key)
-# new_account = update(account, tx)
-# new_addresses = clone(addresses).update(new_account, address) # this is a cheapER clone than full account data
-# new_key = hash([address, tx_root, parent_root])
-# account_db.put(new_key, new_account.serialize())
-# state_root = hash(new_addresses)
-# state_db.put(state_root, new_addresses)
-# block(state_root, parent_root, tx_root)
-
-# state
-# store 1) db with account_hash => account
-#       2) db with state_root => [account_hashs] (to generate the root)
-# hash the account of interest
-# lookup account with account_hash
-# lookup state with state_root
-
-# note: compared to v1 account cloning is reduced to only the modified account
-# however, we always need to clone O(N) account hashes for each modification (... v3?)
-
-
 @dataclass
 class Block:
     parent_hash: bytes
